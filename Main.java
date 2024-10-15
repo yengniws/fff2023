c28221d6662a51da6925bc86af7471fabbb30fef
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-10-15 20:15:23 +0900

    three

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ package f201514199;
 public class Main {
 public static void main(String[] args) {
 System.out.println("one 201514199");
+Three.print();
 }
 }
---
+++ Three.java
@@ -0,0 +1,6 @@
+package f201514199;
+public class Three {
+public static void print() {
+System.out.println("three");
+}
+}
