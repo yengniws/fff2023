f7fb338332624d834d7762ea1d02977a2a35993e
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-10-15 20:13:53 +0900

    two

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package f201514199;
 public class Main {
 public static void main(String[] args) {
+Two.print();
 System.out.println("one 201514199");
 }
 }
---
+++ Two.java
@@ -0,0 +1,6 @@
+package f201514199;
+public class Two {
+public static void print() {
+System.out.println("two");
+}
+}
